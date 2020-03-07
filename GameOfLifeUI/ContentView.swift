0e055bbfa9014c411b9ce7d0c28d137406ba9293
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-03-07 04:25:23 +0100

    Initial Commit

---
+++ GameOfLifeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfLifeUI
+//
+//  Created by Heiko Goes on 07.03.20.
+//  Copyright © 2020 Heiko Goes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
